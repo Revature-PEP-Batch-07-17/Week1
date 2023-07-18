8dce6d836821a1ad270c1f812ffb317cdc7edf9f
Megan Labrecque <megan.labrecque@revature.com>
2023-07-18 09:57:34 -0400

    updated file structure to be by week

---
+++ Day1/CodeEx/Invitation.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Invitation {
+    public static void main(String[] args) {
+        // Allows me to take in user input
+         Scanner scan = new Scanner(System.in);
+
+         adding();
+         System.out.println(adding());
+
+        //prompt user for info
+        System.out.println("Enter a person: ");
+        // save info in variable
+        // DataType name;
+        String person = scan.nextLine();
+
+        System.out.println("Enter an age"); // "\n"
+        int age = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Enter a plural noun: ");
+        String items = scan.nextLine();
+        System.out.println("Enter a plural profession: ");
+        String professions = scan.nextLine();
+        System.out.println("Enter an event: ");
+        String event = scan.nextLine();
+        System.out.println("Enter an adjective: ");
+        String adjective = scan.nextLine();
+        System.out.println("Enter a nickname: ");
+        String nickname = scan.nextLine();
+
+        //create an invitation to give to the user using their input
+        System.out.println();
+        System.out.println("Dear " + person + ",");
+        System.out.println("I want to come to my bday party! I am turning " + age + " years old!");
+        System.out.println("There will be fun things like " +
+                            items + " and " + professions + " at my party. There will even be a " +
+                            event + " event!");
+        System.out.println("Please come to my party this year. If not, I will cry "
+                            + adjective + ".");
+        System.out.println("Sincerely,");
+        System.out.println(nickname);
+
+        scan.close();
+    }
+
+    static int adding(){
+        int sum = 5 + 3;
+        return sum;
+    }
+}
---
+++ Day1/Lesson.md
@@ -0,0 +1,50 @@
+# Day 1: Intro to Java
+
+<img src= "../Java-Basics/Resources/3_tier_model.png"  width="40%" height="30%">  
+
+<br><br>
+  
+## The Big Picture:  
+
+### FullStack
+* Refers to the entire technology stack of the application
+* These developers work with BOTH FrontEnd and BackEnd
+
+### FrontEnd
+* Includes everything that the User/client can view or interact with
+* Developers make user friendly UI and should think about things like accessibility, website/application design and mobile/browser friendly views
+* Main languages used: JavaScript (functionality) and HTML/CSS (actual design)
+
+### BackEnd
+* Includes servers, databases, and other internal architecture that drives the application
+* The user/client will normally never interact with this portion of the application
+* Developers create code that communicates with servers and databases to manipulate/save information to be given to the FrontEnd
+* Main languages used: Java and SQL
+
+<br><br>
+
+## Java as a Language:
+* High level language: 
+    - we don't need to specify every detail of the code, so it is more user-friendly
+* Strongly typed: 
+    - data types are enforced
+* Object Oriented Programming (OOP) Language: 
+    - everything is class based so we work with the class/object and associate methods to them
+* Platform Independent: 
+    - we can run our program on any computer regardless of setup
+* Widely used
+
+<br><br>
+
+## JDK, JRE, JVM Overview:
+
+* JDK (Java Development Kit)
+    - contains: compiler and launcher  
+  
+* JRE (Java Runtime Environment)
+    - contains: libraries and dependencies that the program needs to run  
+  
+* JVM (Java Virtual Machine)
+    - converts byte code (.class file) to machine code (what the computer reads)  
+  
+<br>
