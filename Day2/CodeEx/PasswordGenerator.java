4e72ceca77f584f02faea76eff56945e2d48e175
Megan Labrecque <91804768+labrecquemegan@users.noreply.github.com>
2023-07-19 18:15:15 -0400

    Update PasswordGenerator.java

--- Day2/CodeEx/PasswordGenerator.java
+++ Day2/CodeEx/PasswordGenerator.java
@@ -1,26 +1,52 @@
-import java.util.Scanner;
+public class PasswordGenerator {
+        public static double operatorPasswordPortion(int age) {
+            // Here we will work with the operators first:
+            double num = 0;
 
-public class Main {
-    public static void main(String[] args) {
+            // add 5 to age
+            num = age + 5;
+            System.out.println("After adding 5: " + num);
 
-        // calls the Comparisons class and runs the compareStrings() method
-        Comparisons.compareStrings();
+            num = num * 11;
+            System.out.println("After multiplying by 11: " + num);
+
+            num = num - 20;
+            System.out.println("After subtracting 20: " + num);
+
+            num = num / 2;
+            System.out.println("After dividing by 2: " + num);
+
+            num += num % 3;
+            System.out.println("After adding the remainder: " + num);
+
+            num++;
+            System.out.println("After adding 1: " + num);
+
+            return num;
+        }
 
 
-        // create my scanner object so I can get info from user
-        Scanner scan = new Scanner(System.in);
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-        System.out.println("Lets create a super secret, super secure password!\n");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-        System.out.println("First, tell me your name so we can get started!");
-        // Holds the user input in a String variable called name
-        String name = scan.nextLine();
-        System.out.println("Thank you " + name + ", now, tell me your favorite color (make sure it's atleast 4 characters long!)");
-        String favColor = scan.nextLine();
-        System.out.println("Ahhhh, I see, finally how old are you?");
-        int age = scan.nextInt();
-        System.out.println("Perfect! Hold on while I generate your super secret, super secure password...");
-        System.out.println("Your password is: " + PasswordGenerator.stringMethodsPasswordPortion(name, favColor) + PasswordGenerator.operatorPasswordPortion(age));
-        scan.close();
-    }
+        public static String stringMethodsPasswordPortion(String name, String favColor) {
+            // Now we will work with the String methods
+            favColor = favColor.toUpperCase();
+            System.out.println("After making the string uppercase: " + favColor);
+
+            favColor = favColor.substring(0,3);
+            System.out.println("After taking the substring: " + favColor);
+
+            String secondPart = favColor.concat(name);
+            System.out.println("After using concat() with favColor and name: " + secondPart);
+
+
+            char secondLetterOfName = name.charAt(1);
+            System.out.println("The second letter of name is: " + secondLetterOfName);
+
+            System.out.println("The length f name is: " + name.length());
+
+
+            String newPassword = secondLetterOfName +  "HeLlO" + name.length() + secondPart;
+
+            return newPassword;
+        }
+
 }
